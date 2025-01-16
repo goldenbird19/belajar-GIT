009de081a5c85ea67473a4adba57285dc0283b76
nisa <khairun.nisa.jobseeker@gmail.com>
2025-01-16 13:02:10 +0700

    nyoba revert

--- fungsi.js
+++ fungsi.js
@@ -5,8 +5,5 @@ consule.log(nambah(1,2));
 const kurang(a,b)=>(a-b);
 consule.log(nambah(1,2));
 
-const perkalian(a,b) => (a*b);
-consule.log(perkalian(1,2));
-
-const pembagian(a,b) => (a/b);
-consule.log(pembagian(1,2));
+const perkalianangka(a,b) => (a*b);
+consule.log(perkalianangka(1,2));
